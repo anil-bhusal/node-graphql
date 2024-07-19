54314c0339c64b410c02b0ce5462ef5afe87e1ae
anil-bhusal <anilbhusal0904@gmail.com>
2024-07-19 11:04:14 +0545

    configure the db

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,27 @@ import { createConnection } from 'typeorm';
 import 'reflect-metadata';
 import { schema } from './schema/userSchema';
 import { root } from './resolver/userResolver';
-import config from './ormconfig';
+import config from './config/ormconfig';
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 // TypeORM connection setup
 createConnection(config).then(() => {
-  console.log('Connected to PostgreSQL database');
+    console.log('Connected to PostgreSQL database');
 
-  // Express server setup
-  const app = express();
+    // Express server setup
+    const app = express();
 
-  // GraphQL endpoint
-  app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  }));
+    // GraphQL endpoint
+    app.use('/graphql', graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    }));
 
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/graphql`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}/graphql`);
+    });
 }).catch(error => console.log('Database connection error: ', error));
